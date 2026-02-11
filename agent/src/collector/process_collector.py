71e5f9c40c928cf8f6b1546d8eb5461faf50c98f
Abhay Pandey <abhaypandey2065@gmail.com>
2026-02-11 12:59:33 +0530

    Bug Fix

--- agent/src/collector/process_collector.py
+++ agent/src/collector/process_collector.py
@@ -3,15 +3,17 @@ from utils.config import base_event
 
 def collect_process_activity():
     events = []
-    for proc in psutil.process_iter(['pid', 'name', 'cpu_percentage']):
+    for proc in psutil.process_iter(['pid', 'name']):
         try:
+            cpu_usage = proc.cpu_percent(interval=None)
+
             event = base_event("process_activity")
             event["metadata"] = {
                 "pid": proc.info['pid'],
                 "process_name": proc.info['name'],
-                "cpu_percent": proc.info['cpu_percent']
+                "cpu_percent": cpu_usage
             }
             events.append(event)
-        except (psutil.NoSuchProcess, psutil.AccessDenied):
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             continue
     return events
