09120471e887ac26075ad48f0fcbec8adcf5e4b9
Abhay Pandey <abhaypandey2065@gmail.com>
2026-04-10 08:35:26 +0530

    Bug Fixes

--- agent/src/windows_service.py
+++ agent/src/windows_service.py
@@ -21,6 +21,7 @@ class ThreatTronService(win32serviceutil.ServiceFramework):
         servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE, servicemanager.PYS_SERVICE_STARTED, (self._svc_name, ""))
         thread = threading.Thread(target=run_agent, daemon=True)
         thread.start()
+        self.ReportServiceStatus(win32service.SERVICE_RUNNING)
         win32event.WaitForSingleObject(self.stop_event, win32event.INFINITE)
 
 if __name__ == "__main__":
